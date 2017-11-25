b01926cbd68a8fb29d6bee82d7b6d46f44ee2f1e
mrswonka <kalakolcheg96@mail.ru>
2017-11-25 20:20:04 +0500

    Комментарии

--- src/main/java/com/usatu/servlets/Chebaeva.java
+++ src/main/java/com/usatu/servlets/Chebaeva.java
@@ -21,32 +21,42 @@ public class Chebaeva extends HttpServlet {
     // переопределяем метод doPost, метод срабатывает при отправке запроса методов post на сервер
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Получение значения, переданного с клиента, в виде строки
         String req1Attribute = req.getParameter("km");
         String req2Attribute = req.getParameter("srras");
         String req3Attribute = req.getParameter("rub");
 /*try - ключевое слово, спользуемое для отметки начала блока кода,
- который потенциально может привести к ошибке. finally - отметка для
-блока кода, которому управление передаётся в любом случае*/
+ который потенциально может привести к ошибке. Если операция прошла успешно,
+ то выполняем программу далее до окончания блока try{}
+ Если в строке не было целое число (например было буквы),
+ то произойдет ошибка и сразу же начнет выполняться блок catch{}*/
 try {
 /* Double.parseDouble - конвертация в число, т.к.
-вводимые данные по умолчанию хранятся в виде строки строки
-Для получения параметра из input в JAVA используется метод
-getParameter("name parameter") , в котором указывается имя параметра,
-приведенное в HTML. parseDouble(double i) - возвращает дробное
-число, является методом класса Double */
+вводимые данные хранятся в виде строки*/
         double km = Double.parseDouble(req1Attribute);
 //double число с плавающей точкой
         double srras = Double.parseDouble(req2Attribute);
         double rub = Double.parseDouble(req3Attribute);
         double ras = (srras * km / 100);
         double sum = (ras*rub);
-//String.format Преобразует значения объектов в строки и округляет их до сотых
+
+/*String.format Преобразует значения объектов в строки и округляет их до сотых
+Записываем в виде атрибута полученный результат
+атрибут имеет вид <ключ> <значение>    */
             req.setAttribute("ras", "Вам понадобится - " +  String.format("%.2f",ras) + " л топлива");
             req.setAttribute("sum", "Затраты составят - " +  String.format("%.2f",sum) + " руб.");
         } catch (NumberFormatException exception) {
+            /*
+            Если произошла ошибка при переводе в численный тип,
+            то в атрибут пишем сообщение с ошибкой
+             */
             req.setAttribute("ras",  " Нужно написать цифрами. Попробуйте снова.");
             req.setAttribute("sum",  "");
         }
+         /*
+        Перенаправляем наш запрос на вторую страницу,
+        где собственно и будем выводить наш ответ
+         */
         req.getRequestDispatcher("result.jsp").forward(req, resp);
     }
 }
--- src/main/webapp/result.jsp
+++ src/main/webapp/result.jsp
@@ -4,16 +4,22 @@
     <title>Калькулятор расхода топлива</title>
 </head>
     <body>
+  <!-- описание стиля -->
    <style>
       .fig {
        text-align: center; /* Выравнивание по центру */
         }
    </style>
    <p class="fig">
+<!-- добавление картинки из контекстного пути страницы и установка размеров картинки -->
     <img src="${pageContext.request.contextPath}/image.jpeg" height="50%" class="image-center" />
         </p>
+        <!-- Выводим сообщение, полученное от сервлета,
+         для этого производим поиск по <ключ>-->
         <h1 align="center"> ${ras}</h1>
         <h1 align="center"> ${sum}</h1>
+        <!-- Форма, содержащая в себе кнопку,
+         которая перенаправит нас на исходную страницу-->
         <form action=index.jsp method="POST">
         <p class="fig">    <button type="submit">Заново</button>  </p>
         </form>
